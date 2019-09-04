cd3e40590b6e6084c8165c952d7391b3fee0ece9
root <emd3.entite@gmail.com>
2019-09-04 06:53:05 -0400

    creating models

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,20 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Question extends Model
 {
     //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function replies(){
+        return $this->hasMany(reply::class);
+    }
+    public function categorie(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,19 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
     //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function likes(){
+        return $this->hasMany(like::class);
+    }
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
 }
